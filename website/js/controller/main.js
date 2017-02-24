bcb4ef23d6c48aff8e76bc78e62da47582be422b
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2017-02-24 17:04:28 +0000

    save

--- website/js/controller/main.js
+++ website/js/controller/main.js
@@ -1,203 +1,50 @@
 window.onload = function() {
 
-  var frame_rate = 60;
-  var c = document.getElementById('canvas')
-  var cc = c.getContext('2d')
-  var time = 0
-  var n = 1
+  physx = new PhysicsEngine(120,'canvas','black')
 
-  var objects   = []
-  var forces    = []
-  var materials = []
+  physx.createForce(9.8,['mass'],0,[],'x')
+  physx.createForce(0,[],9.8,['mass'],'y')
+  physx.createForce(-0.1,['height','dx'],-0.1,['width','dy'])
 
-  var update = function(){
-    time++
-    cc.fillStyle = "black";
-    cc.fillRect(0,0,c.width,c.height);
-    applyForces();
-    moveObjects();
-    drawObjects();
-  }
+  physx.createMaterial('rubber',1,0.75,0.75,'orange')
+  physx.createMaterial('steel',25,0.5,0.5,'grey')
 
-  var drawObjects = function() {
-    for (var i = 0; i < objects.length; i++) {
-      var obj = objects[i]
-      cc.fillStyle = "white";
-      cc.fillRect(obj.x,obj.y,obj.width,obj.height);
-    }
-  }
+  physx.createStaticRectangle(0,780,800,25,'steel')
+  physx.createStaticRectangle(0,-5,800,25,'steel')
+  physx.createStaticRectangle(-5,0,25,800,'steel')
+  physx.createStaticRectangle(780,0,25,800,'steel')
 
-  var applyForces = function() {
-    for (var i = 0; i < forces.length; i++) {
-      var f = forces[i]
-      for (var j = 0; j < objects.length; j++) {
-        var obj = objects[j]
-        if (f.x*frame_rate != 1 && obj[f.x_factor] != undefined) {
-          obj.dx += ((f.x * obj[f.x_factor]) / obj.mass)
-        }
-        if (f.y*frame_rate != 1 && obj[f.y_factor] != undefined) {
-          obj.dy += ((f.y * obj[f.y_factor]) / obj.mass)
-        }
+  physx.createCircle(50,50,5,'rubber')
 
-      }
-    }
-  }
+}
 
-  var applyForces2 = function() {
-    for (var i = 0; i < forces.length; i++) {
-      var f = forces[i]
-      for (var j = 0; j < objects.length; j++) {
-        var obj = objects[j]
-        var x_apply = []
-        var y_apply = []
-        for (var k = 0; k < f.x_factor.length; k++) {
-          if (f.x*frame_rate != 1 && obj[f.x_factor[k]] != undefined) {
-            x_apply.push(obj[f.x_factor[k]])
-          }
-        }
-        if (x_apply.length > 0) {
-          var delta_x = f.x
-          for (var k = 0; k < x_apply.length; k++) {
-            delta_x = delta_x*obj[x_apply[k]]
-          }
-          obj.dx += delta_x / obj.mass
-        }
-        for (var k = 0; k < f.y_factor.length; k++) {
-          if (f.y*frame_rate != 1 && obj[f.y_factor[k]] != undefined) {
-            y_apply.push(obj[f.y_factor[k]])
-          }
-        }
-        if (y_apply.length > 0) {
-          var delta_y = f.y
-          for (var k = 0; k < y_apply.length; k++) {
-            delta_y = delta_y*obj[y_apply[k]]
-          }
-          obj.dy += delta_y / obj.mass
-        }
-      }
-    }
-  }
+var test = function() {
+  console.log(physx)
+}
 
-  var moveObjects = function() {
-    applyConstraints();
-    for (var i = 0; i < objects.length; i++) {
-      var obj = objects[i]
-      obj.x += obj.dx;
-      obj.y += obj.dy;
-    }
-  }
-
-  var applyConstraints = function() {
-    for (var i = 0; i < objects.length; i++) {
-      var obj = objects[i]
-      if (obj.x < 0)                        { wall_bounce(edge_l, obj, 'x') }
-      if ((obj.x + obj.width) > c.width)    { wall_bounce(obj, edge_r, 'x') }
-      if ((obj.y + obj.height) > c.height)  { wall_bounce(obj, floor,  'y') }
-    }
-  };
-
-  var Object = function(x,y,height,width,material) {
-    this.id = n++
-    this.x = x;
-    this.y = y;
-    this.dx = 5;
-    this.dy = 0;
-    this.height = height;
-    this.width = width;
-    this.mass = 1;
-    this.bounce = 1;
-    this.friction = 1;
-
-    for (var i = 0; i < materials.length; i++) {
-      if (materials[i].name == material) {
-        this.mass     = materials[i].density*this.height*this.width
-        this.bounce   = materials[i].bounce
-        this.friction = materials[i].friction
-        this.structure = materials[i].structure
-      }
-    }
-  }
-
-  var wall_bounce = function(obj1, obj2, direction) {
-    if (direction == 'x') {
-      obj1.x = obj2.x - obj1.width
-      obj1.dx = 0 - (obj1.dx * obj1.bounce * obj2.bounce)
-      obj1.dy = obj1.dy * obj1.friction * obj2.friction
-      obj2.dx = 0 - (obj2.dx * obj2.bounce * obj1.bounce)
-      obj2.dy = obj2.dy * obj2.friction * obj1.friction
-    }
-    if (direction == 'y') {
-      obj1.y = obj2.y - obj1.height
-      obj1.dy = 0 - (obj1.dy * obj1.bounce * obj2.bounce)
-      obj1.dx = obj1.dx * obj1.friction * obj2.friction
-      obj2.dy = 0 - (obj2.dy * obj2.bounce * obj1.bounce)
-      obj2.dx = obj2.dx * obj2.friction * obj1.friction
-    }
-  }
-
-  var bounce = function(obj1, obj2) {
-
-  }
-
-  var Force = function(x,x_factor,y,y_factor) {
-    this.x = x/frame_rate;
-    this.y = y/frame_rate;
-    this.x_factor = x_factor;
-    this.y_factor = y_factor;
-  }
-
-  var Material = function(name,density,friction,bounce,strength,toughness) {
-    this.name       = name;
-    this.density    = density;
-    this.friction   = friction;
-    this.bounce     = bounce;
-    this.strength   = strength;
-    this.toughness  = toughness;
-  }
-
-  //WORLD CREATION
-  var edge_l = {
-    bounce: 0.99,
-    friction: 0.99,
-    x:0,
-    y:0,
-    dx:0,
-    dy:0
-  };
-  var edge_r = {
-    bounce: 0.99,
-    friction: 0.99,
-    x:c.width,
-    y:0,
-    dx:0,
-    dy:0
-  };
-  var floor = {
-    bounce: 0.99,
-    friction: 0.99,
-    x:0,
-    y:c.height,
-    dx:0,
-    dy:0
-  };
-
-  //MATERIAL CREATION
-  var rubber = new Material('rubber',1,0.95,0.9,0.5,0.9)
-  var metal = new Material('metal',25,0.4,0.25,0.9,0.8)
-  materials.push(rubber)
-  materials.push(metal)
-
-  //FORCES CREATION
-  var gravity = new Force(0,'',9.8,'mass')
-  var air_resistance = new Force(-0.1,'dx',-0.1,'dy')
-  forces.push(air_resistance)
-  forces.push(gravity)
-
-  //OBJECT CREATION
-  var ball = new Object(5,5,10,10,'rubber')
-  var plate = new Object(150,150,5,50,'metal')
-  objects.push(ball)
-  objects.push(plate)
-
-  setInterval(update,1000/frame_rate)
+var x_plus = function() {
+  ct = physx.getConstants()
+  physx.setConstant('x',ct.x+0.5)
+  console.log(ct.x)
+}
+var x_minus = function() {
+  ct = physx.getConstants()
+  physx.setConstant('x',ct.x-0.5)
+  console.log(ct.x)
+}
+var y_plus = function() {
+  ct = physx.getConstants()
+  physx.setConstant('y',ct.y+0.5)
+  console.log(ct.y)
+}
+var y_minus = function() {
+  ct = physx.getConstants()
+  physx.setConstant('y',ct.y-0.5)
+  console.log(ct.y)
+}
+var pause = function() {
+  physx.pause()
+}
+var resume = function() {
+  physx.resume()
 }
---
+++ website/js/controller/physics.js
@@ -0,0 +1,410 @@
+var PhysicsEngine = function(refresh_rate,canvas_name,canvas_colour) {
+
+  var time      = 0
+  var objects   = []
+  var forces    = []
+  var materials = []
+  var n         = 0
+  var c         = document.getElementById('canvas')
+  var cc        = c.getContext('2d')
+  var paused    = false
+
+  var constants = {
+     x  :0,
+     y  :0
+  }
+
+  this.getTime = function() {
+    return time
+  }
+  this.getObjects = function() {
+    return objects
+  }
+  this.getMaterials = function() {
+    return materials
+  }
+  this.getForces = function() {
+    return forces
+  }
+  this.getConstants = function() {
+    return constants
+  }
+  this.setConstant = function(key,value) {
+    constants[key] = value
+  }
+  this.pause = function() {
+    if (!paused) {
+      clearInterval(engine)
+      paused = true
+    }
+  }
+  this.resume = function() {
+    if (paused) {
+      engine = setInterval(update,1000/refresh_rate)
+      paused = false
+    }
+  }
+
+  this.createRectangle = function(x,y,width,height,material) {
+    for (var i = 0; i < materials.length; i++) {
+      if (material == materials[i].name) {
+        var obj = new Rectangle(x,y,width,height,materials[i],false)
+        objects.push(obj)
+      }
+    }
+  }
+  this.createCircle = function(x,y,radius,material) {
+    for (var i = 0; i < materials.length; i++) {
+      if (material == materials[i].name) {
+        var obj = new Circle(x,y,radius,materials[i],false)
+        objects.push(obj)
+      }
+    }
+  }
+  this.createStaticRectangle = function(x,y,width,height,material) {
+    for (var i = 0; i < materials.length; i++) {
+      if (material == materials[i].name) {
+        var obj = new Rectangle(x,y,width,height,materials[i],true)
+        objects.push(obj)
+      }
+    }
+  }
+  this.createMaterial = function(name,density,bounce,friction,colour) {
+    var material = new Material(name,density,bounce,friction,colour)
+    materials.push(material)
+  }
+
+  this.createForce = function(x_scalar,x_factors,y_scalar,y_factors,constant) {
+    var force = new Force(x_scalar,x_factors,y_scalar,y_factors,constant)
+    forces.push(force)
+  }
+
+  var update = function() {
+    time++
+    cc.fillStyle = canvas_colour
+    cc.fillRect(0,0,c.width,c.height)
+    applyForces()
+    moveObjects()
+    drawObjects()
+  }
+
+  var Rectangle = function(x,y,width,height,material,is_static) {
+    this.id = n++
+    this.type = 'rectangle'
+    this.x = x
+    this.y = y
+    this.dx = 0
+    this.dy = 0
+    this.width = width
+    this.height = height
+    this.material = material
+    this.mass = material.density * width * height
+    this.static = is_static
+    if (!is_static) {
+      this.dx = Math.floor(Math.random()*25)
+      this.dy = Math.floor(Math.random()*25)
+    }
+  }
+  var Circle = function(x,y,radius,material,is_static) {
+    this.id = n++
+    this.type = 'circle'
+    this.x = x
+    this.y = y
+    this.dx = 0
+    this.dy = 0
+    this.width = radius
+    this.height = radius
+    this.radius = radius
+    this.material = material
+    this.mass = material.density * radius * radius * Math.PI
+    this.static = is_static
+    if (!is_static) {
+      this.dx = Math.floor(Math.random()*25)
+      this.dy = Math.floor(Math.random()*25)
+    }
+  }
+
+  var Material = function(name,density,bounce,friction,colour) {
+    this.name     = name;
+    this.density  = density;
+    this.bounce   = bounce;
+    this.friction = friction;
+    this.colour   = colour
+  }
+
+  var Force = function(x_scalar,x_factors,y_scalar,y_factors,constant) {
+    this.x = x_scalar;
+    this.fx = x_factors;
+    this.y = y_scalar;
+    this.fy = y_factors;
+    this.z = constant
+  }
+
+  var applyForces = function() {
+    for (var i = 0; i < objects.length; i++) {
+      if (objects[i].static != true) {
+        var obj = objects[i]
+        for (var j = 0; j < forces.length; j++) {
+          var f = forces[j]
+          var apply = true
+          for (var k = 0; k < f.fx.length; k++) {
+            if (typeof obj[f.fx[k]] == undefined) {
+              apply = false
+            }
+          }
+          for (var k = 0; k < f.fy.length; k++) {
+            if (typeof obj[f.fy[k]] == undefined) {
+              apply = false
+            }
+          }
+          if (apply) {
+            var ddx = f.x
+            var ddy = f.y
+            if (typeof constants[f.z] != undefined && f.z != undefined) {
+              ddx *= constants[f.z]
+              ddy *= constants[f.z]
+            }
+            for (var k = 0; k < f.fx.length; k++) {
+              ddx *= obj[f.fx[k]]
+            }
+            for (var k = 0; k < f.fy.length; k++) {
+              ddy *= obj[f.fy[k]]
+            }
+            obj.dx += (ddx/obj.mass)/refresh_rate
+            obj.dy += (ddy/obj.mass)/refresh_rate
+          }
+        }
+      }
+    }
+  }
+
+  var moveObjects = function() {
+    for (var i = 0; i < objects.length; i++) {
+      var obj = objects[i]
+      var cache_y = obj.y
+      var cache_x = obj.x
+      if (obj.dx > 0 && !collide_x_p(obj)) {
+        obj.x += obj.dx
+      }
+      if (obj.dy > 0 && !collide_y_p(obj)) {
+        obj.y += obj.dy
+      }
+      if (obj.dx < 0 && !collide_x_n(obj)) {
+        obj.x += obj.dx
+      }
+      if (obj.dy < 0 && !collide_y_n(obj)) {
+        obj.y += obj.dy
+      }
+      if (obj.y == cache_y) {
+        obj.dy = 0
+      }
+      if (obj.x == cache_x) {
+        obj.dx = 0
+      }
+    }
+  }
+
+  var drawObjects = function() {
+    for (var i = 0; i < objects.length; i++) {
+      var obj = objects[i]
+      cc.fillStyle = obj.material.colour
+      cc.strokeStyle = obj.material.colour
+      if (obj.type == 'circle') {
+        cc.beginPath();
+        cc.arc(obj.x,obj.y,obj.radius,0,2*Math.PI);
+        cc.stroke();
+        cc.fill();
+      } else {
+        cc.fillRect(obj.x,obj.y,obj.width,obj.height)
+      }
+    }
+  }
+
+  var collide_x_p = function(obj) {
+    var collision = false;
+    for (var i = 0; i < objects.length; i++) {
+      if (obj.type == 'circle') {
+        var obj2 = objects[i]
+        if (  obj.x + obj.dx + obj.radius >= obj2.x
+          &&  obj.x + obj.radius <= obj2.x
+          &&  obj.y + obj.radius > obj2.y
+          &&  obj.y - obj.radius < obj2.y + obj2.height
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.x = obj2.x - obj.radius;
+          if (obj2.static == true) {
+            bounceStatic_x(obj,obj2)
+          } else {
+            dynamicBounce_x(obj,obj2)
+          }
+        }
+      } else {
+        var obj2 = objects[i]
+        if (  obj.x + obj.dx + obj.width >= obj2.x
+          &&  obj.x + obj.width <= obj2.x
+          &&  obj.y + obj.height > obj2.y
+          &&  obj.y < obj2.y + obj2.height
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.x = obj2.x - obj.width;
+          if (obj2.static == true) {
+            bounceStatic_x(obj,obj2)
+          } else {
+            dynamicBounce_x(obj,obj2)
+          }
+        }
+      }
+    }
+    return collision
+  }
+
+  var collide_x_n = function(obj) {
+    var collision = false;
+    for (var i = 0; i < objects.length; i++) {
+      var obj2 = objects[i]
+      if (obj.type == 'circle') {
+        if (  obj.x + obj.dx - obj.radius <= obj2.x + obj2.width
+          &&  obj.x + obj.radius >= obj2.x + obj2.width
+          &&  obj.y + obj.radius > obj2.y
+          &&  obj.y - obj.radius < obj2.y + obj2.height
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.x = obj2.x + obj2.width + obj.radius;
+          if (obj2.static == true) {
+            bounceStatic_x(obj,obj2)
+          } else {
+            dynamicBounce_x(obj,obj2)
+          }
+        }
+      } else {
+        if (  obj.x + obj.dx <= obj2.x + obj2.width
+          &&  obj.x >= obj2.x + obj2.width
+          &&  obj.y + obj.height > obj2.y
+          &&  obj.y < obj2.y + obj2.height
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.x = obj2.x + obj2.radius;
+          if (obj2.static == true) {
+            bounceStatic_x(obj,obj2)
+          } else {
+            dynamicBounce_x(obj,obj2)
+          }
+        }
+      }
+    }
+    return collision
+  }
+
+  var collide_y_p = function(obj) {
+    var collision = false;
+    for (var i = 0; i < objects.length; i++) {
+      var obj2 = objects[i]
+      if (obj.type == 'circle') {
+        if (  obj.y + obj.dy + obj.radius >= obj2.y
+          &&  obj.y + obj.radius <= obj2.y
+          &&  obj.x + obj.radius > obj2.x
+          &&  obj.x - obj.radius < obj2.x + obj2.width
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.y = obj2.y - obj.radius;
+          if (obj2.static == true) {
+            bounceStatic_y(obj,obj2)
+          } else {
+            dynamicBounce_y(obj,obj2)
+          }
+        }
+      } else {
+        if (  obj.y + obj.dy + obj.height >= obj2.y
+          &&  obj.y + obj.height <= obj2.y
+          &&  obj.x + obj.width > obj2.x
+          &&  obj.x < obj2.x + obj2.width
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.y = obj2.y - obj.radius;
+          if (obj2.static == true) {
+            bounceStatic_y(obj,obj2)
+          } else {
+            dynamicBounce_y(obj,obj2)
+          }
+        }
+      }
+    }
+    return collision
+  }
+
+  var collide_y_n = function(obj) {
+    var collision = false;
+    for (var i = 0; i < objects.length; i++) {
+      var obj2 = objects[i]
+      if (obj.type == 'circle') {
+        if (  obj.y + obj.dy - obj.radius <= obj2.y + obj2.height
+          &&  obj.y + obj.radius >= obj2.y + obj2.height
+          &&  obj.x + obj.radius > obj2.x
+          &&  obj.x - obj.radius < obj2.x + obj2.width
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.y = obj2.y + obj2.height + obj.radius;
+          if (obj2.static == true) {
+            bounceStatic_y(obj,obj2)
+          } else {
+            dynamicBounce_y(obj,obj2)
+          }
+        }
+      } else {
+        if (  obj.y + obj.dy <= obj2.y + obj2.height
+          &&  obj.y >= obj2.y + obj2.height
+          &&  obj.x + obj.width > obj2.x
+          &&  obj.x < obj2.x + obj2.width
+          &&  obj.id != obj2.id
+        ) {
+          var collision = true;
+          obj.y = obj2.y + obj2.height;
+          if (obj2.static == true) {
+            bounceStatic_y(obj,obj2)
+          } else {
+            dynamicBounce_y(obj,obj2)
+          }
+        }
+      }
+    }
+    return collision
+  }
+
+  var dynamicBounce_x = function(obj1,obj2) {
+    if (obj1.dx > 0 && obj2.dx > 0 || obj1.dx < 0 && obj2.dx < 0) {
+      obj1.dx -= (obj2.dx*obj2.mass*obj2.material.bounce)/obj1.mass
+      obj2.dx += (obj1.dx*obj1.mass*obj2.material.bounce)/obj2.mass
+    } else {
+      obj1.dx += (obj2.dx*obj2.mass*obj2.material.bounce)/obj1.mass
+      obj2.dx += (obj1.dx*obj1.mass*obj1.material.bounce)/obj2.mass
+    }
+    obj2.dy = obj2.dy*obj1.material.friction*obj2.material.friction
+  }
+  var dynamicBounce_y = function(obj1,obj2) {
+    if (obj1.dy > 0 && obj2.dy > 0 || obj1.dy < 0 && obj2.dy < 0) {
+      obj1.dy -= (obj2.dy*obj2.mass*obj2.material.bounce)/obj1.mass
+      obj2.dy += (obj1.dy*obj1.mass*obj2.material.bounce)/obj2.mass
+    } else {
+      obj1.dy += (obj2.dy*obj2.mass*obj2.material.bounce)/obj1.mass
+      obj2.dy += (obj1.dy*obj1.mass*obj1.material.bounce)/obj2.mass
+    }
+    obj1.dx = obj1.dx*obj1.material.friction*obj2.material.friction
+  }
+  var bounceStatic_x = function(obj,s_obj) {
+    obj.dx = 0-(obj.dx * obj.material.bounce * s_obj.material.bounce)
+    obj.dy = obj.dy * obj.material.friction * s_obj.material.friction
+  }
+  var bounceStatic_y = function(obj,s_obj) {
+    obj.dy = 0-(obj.dy * obj.material.bounce * s_obj.material.bounce)
+    obj.dx = obj.dx*obj.material.friction*s_obj.material.friction
+  }
+
+  var engine = setInterval(update,1000/refresh_rate)
+}
