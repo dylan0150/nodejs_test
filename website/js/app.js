b233f19f56b4fd330a565f61343a3abebfd688f5
dylan0150 <dylan.hanner@yahoo.co.uk>
2017-03-03 10:53:34 +0000

    save

--- website/js/app.js
+++ website/js/app.js
@@ -1,1 +1,28 @@
-var app = angular.module('nodeApp', [])
+var app = angular.module('nodeApp', [
+  'ui.router'
+])
+
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('home', {
+        url: '/home',
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl',
+        resolve: {
+
+        }
+      })
+
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'templates/profile.html',
+        controller: 'profileCtrl',
+        resolve: {
+
+        }
+      })
+
+    $urlRouterProvider
+      .otherwise('/home')
+  })
