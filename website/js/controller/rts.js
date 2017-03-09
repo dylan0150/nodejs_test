3e13bbd6515066d2586a9ac2a1b75e877c151be7
dylan0150 <dylan.hanner@yahoo.co.uk>
2017-03-09 22:52:43 +0000

    save

--- website/js/controller/rts.js
+++ website/js/controller/rts.js
@@ -31,7 +31,9 @@ app.controller('rtsCtrl', function($scope,$interval) {
   var loop
   var n = 0
   var terrain = []
-  var terrainTypes = []
+  var terrai_types = []
+  var units = []
+  var unit_types = []
 
   //OBJECTS
   var m = {
@@ -54,7 +56,7 @@ app.controller('rtsCtrl', function($scope,$interval) {
   var cam = {
     x     : 0,
     y     : 0,
-    speed : 1,
+    speed : 3,
     lock  : false
   }
 
@@ -96,21 +98,48 @@ app.controller('rtsCtrl', function($scope,$interval) {
 
   //CLASSES
   var Terrain = function(type,x,y,width,height) {
+    this.class = 'Terrain'
     this.id = n++
     this.x = x
     this.y = y
     this.width = width
     this.height = height
-    this.type = { name: 'null', colour: 'white' }
+    this.type = { name: 'null', colour: 'white', passable: 0 }
     for (var i = 0; i < terrainTypes.length; i++) {
       if (type == terrainTypes[i].name) {this.type == terrainTypes[i]}
     }
     terrain.push(this)
   }
   var TerrainType = function(name,colour) {
+    this.class = 'TerrainType'
+    this.id = n++
     this.name = name
     this.colour = colour
-    terrainTypes.push(this)
+    terrain_types.push(this)
+  }
+  var Unit = function(type,x,y,width,height) {
+    this.class = 'Unit'
+    this.id = n++
+    this.x = x
+    this.y = y
+    this.dx = 1
+    this.dy = 1
+    this.width = width
+    this.height = height
+    this.type = { name: 'null', colour: 'white', density: 1 }
+    for (var i = 0; i < unit_types.length; i++) {
+      if (type == unit_types[i].name) {this.type == unit_types[i]}
+    }
+    this.mass = width * height * this.type.density
+    units.push(this)
+  }
+  var UnitType = function(name,colour,density) {
+    this.class = 'UnitType'
+    this.id = n++
+    this.name = name
+    this.colour = colour
+    this.density = density
+    unit_types.push(this)
   }
 
   //LOOP
@@ -118,11 +147,13 @@ app.controller('rtsCtrl', function($scope,$interval) {
     frame++
     drawBackground()
     drawTerrain()
+    drawUnits()
     if (!$scope.paused) { window.requestAnimationFrame(draw) }
   }
   var update = function(){
     time++
     moveCamera()
+    moveUnits()
   }
   var start = function(){
     loop = $interval(update,1000/refresh)
@@ -140,18 +171,97 @@ app.controller('rtsCtrl', function($scope,$interval) {
   var drawTerrain = function() {
     for (var i = 0; i < terrain.length; i++) {
       var t = terrain[i]
-      ctx.fillStyle = t.colour
-      ctx.fillRect(t.x,t.y,t.width,t.height)
+      ctx.fillStyle = t.type.colour
+      ctx.fillRect(t.x-cam.x,t.y-cam.y,t.width,t.height)
+    }
+  }
+  var drawUnits = function() {
+    for (var i = 0; i < units.length; i++) {
+      var u = units[i]
+      ctx.fillStyle = u.type.colour
+      ctx.fillRect(u.x-cam.x,u.y-cam.y,u.width,u.height)
     }
   }
 
   //UPDATE
   var moveCamera = function() {
     if (!cam.lock) {
-      if (k.up)     { cam.y += cam.speed }
-      if (k.down)   { cam.y -= cam.speed }
+      if (k.up)     { cam.y -= cam.speed }
+      if (k.down)   { cam.y += cam.speed }
       if (k.right)  { cam.x += cam.speed }
       if (k.left)   { cam.x -= cam.speed }
+    }
+  }
+  var moveUnits = function() {
+    for (var i = 0; i < units.length; i++) {
+      var u = units[i]
+      if( !collision_x(u,terrain) && !collision_x(u,units) ) { u.x += u.dx }
+      if( !collision_y(u,terrain) && !collision_y(u,units) ) { u.y += u.dy }
+    }
+  }
+  var collision_x = function(u,array) {
+    var collide = false
+    for (var i = 0; i < array.length; i++) {
+      var t = array[i]
+      if ( u.x > 0
+        && u.x + u.width < t.x
+        && u.x + u.dx + u.width >= t.x
+        && u.y <= t.y + t.height
+        && u.y + u.height >= t.y
+      ) {
+        collide = true
+        event_Collision(u,t,'x+')
+      }
+      if ( u.x < 0
+        && u.x > t.x + t.width
+        && u.x + u.dx <= t.x + t.width
+        && u.y <= t.y + t.height
+        && u.y + u.height >= t.y
+      ) {
+        collide = true
+        event_Collision(u,t,'x-')
+      }
+    }
+    return collide
+  }
+  var collision_y = function(u,array) {
+    var collide = false
+    for (var i = 0; i < array.length; i++) {
+      var t = array[i]
+      if ( u.y > 0
+        && u.y + u.height < t.y
+        && u.y + u.dy + u.height >= t.y
+        && u.x <= t.x + t.width
+        && u.x + u.width >= t.x
+      ) {
+        collide = true
+        event_Collision(u,t,'y+')
+      }
+      if ( u.y < 0
+        && u.y > t.y + t.height
+        && u.y + u.dy <= t.y + t.height
+        && u.x <= t.x + t.width
+        && u.x + u.width >= t.x
+      ) {
+        collide = true
+        event_Collision(u,t,'y-')
+      }
+    }
+    return collide
+  }
+
+  //EVENTS
+  event_Collision = function(u,u2,direction) {
+    if (u.class == 'Unit' && u2.class == 'Terrain') {
+      if (u2.type.passable == 0) {
+        if (direction == 'x+') {u.x = u2.x - u.width}
+        if (direction == 'x-') {u.x = u2.x + u2.width}
+        if (direction == 'y+') {u.y = u2.y - u.height}
+        if (direction == 'y-') {u.y = u2.y + u2.height}
+      } else {
+        if (direction == 'x+' || direction == 'x-') {u.dx *= u2.type.passable; u.x += u.dx }
+        if (direction == 'y+' || direction == 'y-') {u.dy *= u2.type.passable; u.y += u.dy }
+      }
     }
   }
 
