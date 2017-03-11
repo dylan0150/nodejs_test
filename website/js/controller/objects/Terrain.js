83c19a9c4f509f1e252bc6f07cd4204ba74a3338
dylan0150 <dylan.hanner@yahoo.co.uk>
2017-03-11 20:14:25 +0000

    save

--- website/js/controller/objects/Terrain.js
+++ website/js/controller/objects/Terrain.js
@@ -1,4 +1,10 @@
 var Terrain = function( world, logic, collide, options ) {
+  this.animation  = { empty:true, current:'null' }
+  this.image      = ""
+  if (this.image != "") {
+    this.img      = new Image()
+    this.img.src  = this.image
+  }
   this.class      = 'Terrain'
   this.time       = 0
   this.collision  = true
@@ -17,10 +23,21 @@ var Terrain = function( world, logic, collide, options ) {
   this.update = function () {
     this.time++
     if (typeof logic == 'function') { logic(this) }
-  };
+  }
   this.show = function () {
-    ctx.fillStyle = this.colour
-    ctx.fillRect(this.x-cam.x-this.width/2,this.y-cam.y-this.height/2,this.width,this.height)
-  };
+    if (!this.animation.empty) {
+      var a = this.animation[this.animation.current]
+      if (this.time % a.speed == 0)   { a.step++ }
+      if (a.step >= a.frames.length)  { a.step = 0 }
+      var lx = a.frames[a.step][0]
+      var ly = a.frames[a.step][1]
+      ctx.drawImage(a.img,lx*a.w,ly*a.h,a.w,a.h,this.x-cam.x-this.width/2,this.y-cam.y-this.height/2,this.width,this.height)
+    } else if (this.image != "") {
+      ctx.drawImage(this.img,this.x-cam.x-this.width/2,this.y-cam.y-this.height/2,this.width,this.height)
+    } else {
+      ctx.fillStyle = this.colour
+      ctx.fillRect(this.x-cam.x-this.width/2,this.y-cam.y-this.height/2,this.width,this.height)
+    }
+  }
   world.add(this)
 };
