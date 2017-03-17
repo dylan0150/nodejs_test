7df2091424a0563cbafe1bb7505e1a0efe6cb990
Dylan Hanner <dylan.hanner@yahoo.co.uk>
2017-03-17 16:17:34 +0000

    save

--- server/api/user.js
+++ server/api/user.js
@@ -1,1 +1,14 @@
-exports.get = function(params,data) {}
+var fs = require('fs')
+var config = require('./../config')
+
+exports.get = function(params,data,cookie) {
+  var json = JSON.parse(fs.readFileSync(config.path.index+'server/user.json'))
+  var data = {}
+  for (var i = 0; i < json.users.length; i++) {
+    var user = json.users[i]
+    if (user.id == cookie.id) {
+      data = user.data
+    }
+  }
+  return {ok:true, user:data}
+}
